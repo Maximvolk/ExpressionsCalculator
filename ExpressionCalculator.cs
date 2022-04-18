041976ac01b16e9fef46a24b3f4825f955ea870e
Максим Волков <maximvolk@Maksims-MacBook-Pro.local>
2022-04-18 22:08:59 +0300

    Implement expression splitting on tokens

--- ExpressionCalculator.cs
+++ ExpressionCalculator.cs
@@ -5,27 +5,60 @@ using System.Text;
 
 namespace ExpressionsCalculation
 {
-    class ExpressionCalculator
+    public class ExpressionCalculator
     {
         // Calculate expression using Bauer - Zamelson algorithm and Inverse Polish notation
-        static double Calculate(string expression)
+        public static double Calculate(string expression)
         {
-            var expressionList = new List<object>();
+            var expressionItems = SplitExpression(expression);
 
-
-            ConvertToInversePolishNotation(expressionList);
-
-            return ComputeInversePolishExpression(expressionList);
+            
+            return 0;
         }
 
-        private static List<object> ConvertToInversePolishNotation(List<object> expression)
+        private static List<object> SplitExpression(string expression)
         {
+            var operators = new List<char> { '(', ')', '+', '-', '*', '/' };
+            var floatSeparators = new List<char> { ',', '.' };
 
-        }
+            var expressionItems = new List<object>();
+            object currentNumber = null;
+            bool floatingPointOccurred = false;
 
-        private static double ComputeInversePolishExpression(List<object> expression)
-        {
+            foreach (var symbol in expression)
+            {
+                if (operators.Contains(symbol))
+                {
+                    if (currentNumber != null)
+                        expressionItems.Add(currentNumber);
+                    
+                    expressionItems.Add(symbol);
 
+                    floatingPointOccurred = false;
+                    currentNumber = null;
+                }
+                else if (char.IsDigit(symbol))
+                {
+                    // Case when symbol is probably a part of floating point number
+                    if (floatingPointOccurred)
+                        currentNumber = double.Parse($"{currentNumber}.{symbol}");
+                    else
+                        currentNumber = double.Parse($"{currentNumber}{symbol}");
+                    
+                    floatingPointOccurred = false;
+                }
+                else if (floatSeparators.Contains(symbol))
+                    floatingPointOccurred = true;
+
+                else if (symbol != ' ')
+                    throw new Exception($"Invalid symbol occurred: {symbol}");
+            }
+
+            // Do not forget last item if it exists
+            if (currentNumber != null)
+                expressionItems.Add(double.Parse(currentNumber.ToString()));
+
+            return expressionItems;
         }
     }
 }
